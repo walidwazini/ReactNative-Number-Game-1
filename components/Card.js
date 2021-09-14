5af8c301869918be768ed0d4655ee50495cfc91e
Walid Wazini <wazini25@gmail.com>
2021-09-14 14:32:55 +0800

    Card comonent

---
+++ components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, StyleSheet, Button, Text, } from 'react-native'
+
+const Card = (props) => {
+    return (
+        <View style={{...styles.card, ...props.style}} >
+            {props.children}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        // width: 300,
+        // maxWidth: '80%',
+        // alignItems: 'center',
+        shadowColor: 'purple',
+        shadowOffset: { width: 0, height: 2, },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        padding: 18,
+        borderRadius: 14
+    }
+})
+
+export default Card
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,54 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+
+import Card from '../components/Card'
 
 
 const StartGameScreen = () => {
     return (
         <View style={styles.screen} >
-            <Text>Game Screen</Text>
+            <Text style={styles.title} >Game Screen</Text>
+            <Card style={styles.inputWrapper} >
+                <Text>Select a Number</Text>
+                <TextInput style={styles.numberInput} />
+                <View style={styles.buttonWrapper} >
+                    <View>
+                        <Button style={styles.button} title='Reset' onPress={() => { }} color='grey' />
+                    </View>
+                    <View>
+                        <Button style={styles.button} title='Confirm' onPress={() => { }} color='#c717fc' />
+                    </View>
+                </View>
+            </Card>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex : 1,
+    screen: {
+        flex: 1,
         padding: 10,
         alignItems: 'center',
+    },
+    title: {
+        fontSize: 30,
+        marginVertical: 12
+    },
+    inputWrapper: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    buttonWrapper: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+    button : {
+        width: 105,
     }
+
 })
 
 export default StartGameScreen
